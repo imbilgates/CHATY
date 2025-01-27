3309756c046abb3dfb960df02ea7e6d19c2de788
bilgates27 <bilgates270703@gmail.com>
2025-01-27 14:07:17 +0530

    update InfoBar component to use local delete icon image

--- client/src/components/InfoBar.js
+++ client/src/components/InfoBar.js
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react';
 import closeIcon from '../icons/closeIcon.png';
-import deleteIcon from '../icons/deleteicon.png';
 import SimpleDialog from '../MUI/SimpleDialog';
 import Avatar from '@mui/material/Avatar';
 import { StyledBadge } from '../MUI/MaterialUIStyled';
@@ -84,7 +83,7 @@ const InfoBar = ({ users, name, room, image, deleteMessages }) => {
         <FormControlLabel onClick={handleTheme}
           control={<MaterialUISwitch sx={{ m: 1 }} defaultChecked />}
         />
-        <img onClick={handleDeleteDialogOpen} alt='delete icon' src={deleteIcon} style={{ width: '35px', height: '35px', marginRight: '20px' }}>
+        <img onClick={handleDeleteDialogOpen} alt='delete icon' src={"./delete.png"} style={{ width: '35px', height: '35px', marginRight: '20px' }}>
         </img>
         <div onClick={handleLeaveChatDialogOpen}>
           <img src={closeIcon} alt="close icon" style={{ width: '15px', height: '15px', marginRight: '5px' }} />
